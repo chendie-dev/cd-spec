f28edc74d86d809a4a05595e63e5bae027e87081
chendie <chendie>
2023-08-19 16:48:24 +0800

    fix: git分支修改

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@
 set -e
 
 
-push_addr=`git remote get-url --push origin`
-commit_info=`git describe --all --always --long`
+push_addr=`git remote get-url --push origin` #获取远程仓库地址
+commit_info=`git describe --all --always --long` #最近的一次提交
 dist_path=docs/.vuepress/dist
 push_branch=gh-pages
 
@@ -18,7 +18,7 @@ cd $dist_path
 git init
 git add -A
 git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git push -f $push_addr HEAD:$push_branch #push并修改远程分支
 
 cd -
 rm -rf $dist_path
