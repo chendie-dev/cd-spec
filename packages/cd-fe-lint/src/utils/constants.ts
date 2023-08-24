f182fd9a2e14005d54ef987d37039595c5881c7c
chendie <chendie>
2023-08-24 14:31:26 +0800

    fix: project type

--- packages/cd-fe-lint/src/utils/constants.ts
+++ packages/cd-fe-lint/src/utils/constants.ts
@@ -69,6 +69,56 @@ export const MARKDOWN_LINT_IGNORE_PATTERN: string[] = [
 ];
 
 /**
+ * 项目类型
+ */
+export const PROJECT_TYPES: Array<{ name: string; value: string }> = [
+  {
+    name: '未使用 React、Vue、Node.js 的项目（JavaScript）',
+    value: 'index',
+  },
+  {
+    name: '未使用 React、Vue、Node.js 的项目（TypeScript）',
+    value: 'typescript',
+  },
+  {
+    name: 'React 项目（JavaScript）',
+    value: 'react',
+  },
+  {
+    name: 'React 项目（TypeScript）',
+    value: 'typescript/react',
+  },
+  {
+    name: 'Rax 项目（JavaScript）',
+    value: 'rax',
+  },
+  {
+    name: 'Rax 项目（TypeScript）',
+    value: 'typescript/rax',
+  },
+  {
+    name: 'Vue 项目（JavaScript）',
+    value: 'vue',
+  },
+  {
+    name: 'Vue 项目（TypeScript）',
+    value: 'typescript/vue',
+  },
+  {
+    name: 'Node.js 项目（JavaScript）',
+    value: 'node',
+  },
+  {
+    name: 'Node.js 项目（TypeScript）',
+    value: 'typescript/node',
+  },
+  {
+    name: '使用 ES5 及之前版本 JavaScript 的老项目',
+    value: 'es5',
+  },
+];
+
+/**
  * Prettier 扫描文件扩展名
  */
 export const PRETTIER_FILE_EXT = [
